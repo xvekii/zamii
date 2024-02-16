a71563966f30837a0d808ea4aa2ddb9fb8177bba
xvekii <vedran.dautanec@gmail.com>
2024-02-16 07:06:22 +0100

    Execute SELECT on the db to store ime and prezime in popis_ucitelja

--- zamii.py
+++ zamii.py
@@ -6,3 +6,6 @@ popis_ucitelja = []
 
 db_connection = sqlite3.connect("ucitelji.db")
 db = db_connection.cursor()
+
+db.execute("SELECT prezime, ime FROM ucitelji ORDER BY prezime")
+rows = db.fetchall()
